eba189c4c0bc2ee0ba4ca1ea4c4e119583599e38
JasonAsare <mypaapa.asare@gmail.com>
2025-09-08 16:41:45 +0000

    commited

---
+++ nested_if_statement.py
@@ -0,0 +1,24 @@
+AGE = 18
+IS_RICH = True
+IS_CUTE = True
+IS_POPULAR = True
+ACCEPTED_CHRIST = True
+
+User_age = int(input("Age: "))
+
+if(User_age>= AGE):
+    if(IS_RICH):
+        if(IS_CUTE):
+            if(IS_POPULAR):
+                if(ACCEPTED_CHRIST):
+                    print("Come to Daddy")
+                else:
+                    ("Accept Christ")
+            else:
+                ("Become popular")
+        else:
+            ("Become Cute")
+    else:
+        ("Print Money")
+else:
+    ("Too young to see daddy")
