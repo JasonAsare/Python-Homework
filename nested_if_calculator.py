029fd0b2a274c59a922052a40652829f76dfd2d1
JasonAsare <mypaapa.asare@gmail.com>
2025-09-09 21:47:35 +0000

    Commited

---
+++ nested_if_calculator.py
@@ -0,0 +1,39 @@
+"""
+Nested if calculator
+"""
+
+num1 = float(input("Enter number: "))
+num2 = float(input("Enter number: "))
+
+print("Select operation:")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+print("5. Squared (**)")
+print("6. Modulus (%)")
+
+operation = input("+, -, *, /, **, %: ")
+
+if operation == "+":
+    print('The answer is', num1 + num2)
+else:
+    if operation == "-":
+        print("The number is", num1 - num2)
+    else:
+        if operation == "*":
+            print("The number is", num1 * num2)
+        else:
+            if operation == "/":
+                print("The number is", num1 / num2)
+            else:
+                if operation == "**":
+                    print("The number is", num1 ** num2)
+                else:
+                    if operation == "%":
+                        print("The number is", num1 % num2)
+                    else:
+                        print("Invalid key")
+            
+
+    
